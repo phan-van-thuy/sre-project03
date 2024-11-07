cac350beeeafbf24e28b399299c7824656cd9700
thuyphanv <thuyphanv@gmail.com>
2024-11-07 16:34:09 +0700

    edit cicd

--- starter/infra/eks.tf
+++ starter/infra/eks.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
-  config_path            = "/tmp/kubeconfig/config" #"~/.kube/config"
+  config_path            = "/tmp/config" #"~/.kube/config"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
